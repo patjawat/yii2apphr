1192fa8bae942c43066a73437db85dad7729f6e2
patjawat <patjawat@gmail.com>
2022-04-29 09:48:10 +0700

    update

--- controllers/LiffController.php
+++ controllers/LiffController.php
@@ -1,13 +1,27 @@
 <?php
 
 namespace app\controllers;
+use Yii;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\web\Response;
+
+use app\models\Person;
+use app\models\PersonSearch;
 
 class LiffController extends \yii\web\Controller
 {
     public function actionIndex()
     {
         $this->layout = 'line';
-        return $this->render('index');
+        $searchModel = new PersonSearch();
+        $dataProvider = $searchModel->search($this->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
     }
 
 }
